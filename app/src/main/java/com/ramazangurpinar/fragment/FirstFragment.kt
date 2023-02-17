d31e302382529818d14aab9b239bc0ebc18b27ce
Ramazan Gürpınar <ramazan.gurpinar.1997@hotmail.com>
2023-02-17 23:53:55 +0300

    First Fragment bağlama

--- app/src/main/java/com/ramazangurpinar/fragment/FirstFragment.kt
+++ app/src/main/java/com/ramazangurpinar/fragment/FirstFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ramazangurpinar.fragment.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +21,24 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            next(it)
+        }
     }
 
     fun next(view: View){
         println("next tıklandı")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
